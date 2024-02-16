2428da4ca7d8551098cf35ff0455f7badad592e7
arifsadiq <arifsadiq@gmail.com>
2024-02-16 14:36:51 +0400

    Final files

--- script.js
+++ script.js
@@ -1,46 +1,21 @@
-let currentInput = '';
-let operator = '';
-let result = '';
+let displayValue = '';
 
-function appendNumber(num) {
-    currentInput += num;
-    updateDisplay();
-}
-
-function appendOperator(op) {
-    operator = op;
-    result = currentInput;
-    currentInput = '';
-    updateDisplay();
-}
-
-function calculate() {
-    let num1 = parseFloat(result);
-    let num2 = parseFloat(currentInput);
-    switch(operator) {
-        case '+':
-            currentInput = num1 + num2;
-            break;
-        case '-':
-            currentInput = num1 - num2;
-            break;
-        case '*':
-            currentInput = num1 * num2;
-            break;
-        case '/':
-            currentInput = num1 / num2;
-            break;
-    }
-    updateDisplay();
+function appendToDisplay(value) {
+    displayValue += value;
+    document.getElementById('display').value = displayValue;
 }
 
 function clearDisplay() {
-    currentInput = '';
-    result = '';
-    operator = '';
-    updateDisplay();
+    displayValue = '';
+    document.getElementById('display').value = displayValue;
 }
 
-function updateDisplay() {
-    document.getElementById('display').value = currentInput;
+function calculate() {
+    try {
+        displayValue = eval(displayValue);
+        document.getElementById('display').value = displayValue;
+    } catch (error) {
+        displayValue = '';
+        document.getElementById('display').value = 'Error';
+    }
 }
